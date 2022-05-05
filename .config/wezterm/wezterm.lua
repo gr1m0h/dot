9f7db5096e1339e332c6dfba321ea0ffd0d622d1
grimoh <dev@grimoh.net>
2022-05-06 00:01:00 +0900

    Add wezterm config file

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,10 @@
+local wezterm = require 'wezterm';
+
+return {
+	color_scheme = "Dracula",
+	keys = {
+    		-- This will create a new split and run your default program inside it
+    		{key = "d", mods= "CMD",
+			action=wezterm.action{SplitHorizontal={domain="CurrentPaneDomain"}}},
+	}
+}
