aa208880453d7ee51405d86d8bf06aecda29c990
grimoh <dev@grimoh.net>
2022-05-09 11:30:21 +0900

    Remove unnecessary files

--- .config/nvim/config/markdown-preview.lua
+++
@@ -1,3 +0,0 @@
--- https://github.com/iamcco/markdown-preview.nvim
-
-vim.api.nvim_set_keymap('n', 'pv', ':MarkdownPreview<Return>', { noremap = true })
--- .config/nvim/config/nvim-autopairs.lua
+++
@@ -1,6 +0,0 @@
--- https://github.com/windwp/nvim-autopairs
-
-require('nvim-autopairs').setup {
-	-- map the <CR> key
-	map_cr = false,
-}
