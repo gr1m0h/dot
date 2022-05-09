8f60a0b4e80cc7ba2b7f305f2dc5f36878b51b1f
grimoh <dev@grimoh.net>
2022-05-09 11:03:04 +0900

    Update wezterm.lua

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -4,7 +4,7 @@ return {
 	color_scheme = "Dracula",
 	keys = {
     		-- This will create a new split and run your default program inside it
-    		{key = "d", mods= "CMD",
-			action=wezterm.action{SplitHorizontal={domain="CurrentPaneDomain"}}},
+    		{ key = 'd', mods = 'CMD',
+          action = wezterm.action{ SplitHorizontal = { domain = 'CurrentPaneDomain' }}},
 	}
 }
