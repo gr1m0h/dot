c049dda2fe595ebccd6afbd67ddee1c3402a1eb1
grimoh <dev@grimoh.net>
2022-07-06 23:18:19 +0900

    Add .config/nvim/lua/config/nvim-lspconfig.lua

---
+++ .config/nvim/lua/config/nvim-lspconfig.lua
@@ -0,0 +1,22 @@
+-- https://github.com/neovim/nvim-lspconfig
+
+local config = require('lspconfig')
+
+-- bashls
+-- dockerls
+-- gopls
+config.gopls.setup{}
+-- jsonls
+config.jsonls.setup{}
+-- quick_lint_js
+config.quick_lint_js.setup{}
+-- sumneko_lua
+config.sumneko_lua.setup{}
+-- terraformls
+config.terraformls.setup{}
+-- tsserver
+config.tsserver.setup{}
+-- yamlls
+config.yamlls.setup{}
+-- zk
+config.zk.setup{}
