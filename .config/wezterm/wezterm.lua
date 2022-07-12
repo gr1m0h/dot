40140ee03109a26647d6a159d38b82168d13a206
grimoh <dev@grimoh.net>
2022-07-12 12:47:32 +0900

    Update wezterm.lua

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,7 +1,8 @@
 local wezterm = require 'wezterm';
 
 return {
-	color_scheme = "Dracula",
+	color_scheme = 'Dracula',
+	window_background_opacity = 0.8,
 	keys = {
     		-- This will create a new split and run your default program inside it
     		{ key = 'd', mods = 'CMD',
