3ce65803e2e73ff66fb0d609974456c0bbcc945c
grimoh <dev@grimoh.net>
2022-07-13 07:37:13 +0900

    feat: Delete LSP Server config

--- .config/nvim/lua/config/nvim-lspconfig.lua
+++ .config/nvim/lua/config/nvim-lspconfig.lua
@@ -2,23 +2,15 @@
 
 local config = require('lspconfig')
 
--- bashls
-config.bashls.setup{}
 -- dockerls
 config.dockerls.setup{}
 -- gopls
 config.gopls.setup{}
--- jsonls
-config.jsonls.setup{}
 -- quick_lint_js
 config.quick_lint_js.setup{}
--- sumneko_lua
-config.sumneko_lua.setup{}
 -- terraformls
 config.terraformls.setup{}
 -- tsserver
 config.tsserver.setup{}
 -- yamlls
 config.yamlls.setup{}
--- zk
-config.zk.setup{}
