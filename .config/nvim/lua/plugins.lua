f140d43630b67b549d67a162cc786c913153826e
grimoh <dev@grimoh.net>
2022-07-28 21:43:48 +0900

    feat: Add Neovim plugin "hop.nvim"

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -202,6 +202,20 @@ function()
   }
   -------------------------------
   --
+  -- Moving
+  --
+  -------------------------------
+  -- motions on speed!
+  use {
+    'phaazon/hop.nvim',
+    branch = 'v2',
+    config = function()
+      -- you can configure Hop the way you like here; see :h hop-config
+      require'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
+    end,
+  }
+  -------------------------------
+  --
   -- Editing
   --
   -------------------------------
