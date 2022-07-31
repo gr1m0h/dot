f7f9298faa87b87d67edb6319cd389cb3ca0174d
grimoh <dev@grimoh.net>
2022-07-31 13:12:05 +0900

    feat: Update config.fish
    Using fish-prompt: https://github.com/grimoh/fish-prompt

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,50 +4,17 @@ set -x fish_greeting
 # vi mode
 fish_vi_key_bindings
 
+# enable grimoh/fish-prompt
 function fish_mode_prompt
 end
 
-# prompt
-function fish_prompt
-  if test "$fish_key_bindings" = "fish_vi_key_bindings"
-    switch $fish_bind_mode
-      case default
-        set_color bd93f9 purple
-      case insert
-        set_color 50fa7b green
-      case replace_one
-        set_color ffb86c magenta
-      case replace
-        set_color ffb86c magenta
-      case visual
-        set_color f1fa8c yellow
-      end
-    echo (prompt_pwd) "> "
-  end
-end
-
-# output git branch
-function git_branch
-  git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ [\1]/'
-end
-
-# right side prompt
-function fish_right_prompt
-  set_color ff79c6 brred
-  echo (git_branch)
-end
-
-# util
 alias ..='cd ..'
 alias ...='cd ../..'
 alias ....='cd ../../..'
 alias c='clear'
-
-set -gx EDITOR $HOME/.asdf/shims/nvim
-set -gx XDG_DATA_HOME $HOME/.local/share
-
-# rm
 alias rm='rubbish chuckout'
+
+set -gx XDG_DATA_HOME $HOME/.local/share
 
 # asdf
 source /opt/homebrew/opt/asdf/libexec/asdf.fish
@@ -55,6 +22,8 @@ source /opt/homebrew/opt/asdf/libexec/asdf.fish
 # aqua
 set -gx AQUA_ROOT_DIR $XDG_DATA_HOME/aquaproj-aqua
 set -gx PATH $AQUA_ROOT_DIR/bin $PATH
+
+set -gx EDITOR $AQUA_ROOT_DIR/bin/nvim
 
 # kubectl
 alias k='kubectl'
