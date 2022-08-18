2faa886cf9130ac7be58dbd08ce5adff0d5040a7
grimoh <dev@grimoh.net>
2022-08-19 01:34:44 +0900

    Remove nvim-autopairs.lua and nvim-lsp-installer.lua

--- .config/nvim/lua/config/nvim-autopairs.lua
+++
@@ -1,9 +0,0 @@
--- https://github.com/windwp/nvim-autopairs
-
-local status, autopairs = pcall(require, 'nvim-autopairs')
-if (not status) then return end
-
-autopairs.setup {
-  -- map the <CR> key
-  map_cr = false,
-}
