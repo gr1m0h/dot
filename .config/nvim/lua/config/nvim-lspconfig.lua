ef32ee06ff39ec7af649309565147f5cce9d30e0
grimoh <dev@grimoh.net>
2022-10-27 01:01:58 +0900

    Update gopls setting

--- .config/nvim/lua/config/nvim-lspconfig.lua
+++ .config/nvim/lua/config/nvim-lspconfig.lua
@@ -20,6 +20,7 @@ config.sumneko_lua.setup{
 config.dockerls.setup{}
 -- go
 config.gopls.setup{
+  autostart = true,
   cmd = { 'gopls', 'serve' },
   settings = {
     gopls = {
@@ -34,31 +35,51 @@ config.gopls.setup{
   },
 }
 
-vim.api.nvim_create_autocmd("BufWritePre", {
+local goimports = function(timeout_ms)
+  local context = { source = { organizeImports = true } }
+  vim.validate { context = { context, "t", true } }
+
+  local params = vim.lsp.util.make_range_params()
+  params.context = context
+
+  -- See the implementation of the textDocument/codeAction callback
+  -- (lua/vim/lsp/handler.lua) for how to do this properly.
+  local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, timeout_ms)
+  if not result or next(result) == nil then return end
+  local actions = result[1].result
+  if not actions then return end
+  local action = actions[1]
+
+  -- textDocument/codeAction can return either Command[] or CodeAction[]. If it
+  -- is a CodeAction, it can have either an edit, a command or both. Edits
+  -- should be executed first.
+  if action.edit or type(action.command) == "table" then
+    if action.edit then
+      vim.lsp.util.apply_workspace_edit(action.edit)
+    end
+    if type(action.command) == "table" then
+      vim.lsp.buf.execute_command(action.command)
+    end
+  else
+    vim.lsp.buf.execute_command(action)
+  end
+end
+
+vim.api.nvim_create_autocmd(
+  {"BufWritePre"}, {
   pattern = { "*.go" },
   callback = function()
 	  vim.lsp.buf.formatting_sync(nil, 3000)
   end,
 })
 
--- vim.api.nvim_create_autocmd("BufWritePre", {
--- 	pattern = { "*.go" },
--- 	callback = function()
--- 		local params = vim.lsp.util.make_range_params(nil, vim.lsp.util._get_offset_encoding())
--- 		params.context = {only = {"source.organizeImports"}}
---
--- 		local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
--- 		for _, res in pairs(result or {}) do
--- 			for _, r in pairs(res.result or {}) do
--- 				if r.edit then
--- 					vim.lsp.util.apply_workspace_edit(r.edit, vim.lsp.util._get_offset_encoding())
--- 				else
--- 					vim.lsp.buf.execute_command(r.command)
--- 				end
--- 			end
--- 		end
--- 	end,
--- })
+vim.api.nvim_create_autocmd(
+  {"BufWritePre"}, {
+  pattern = { "*.go" },
+  callback = function()
+	  goimports(1000)
+  end,
+})
 
 -- js
 config.quick_lint_js.setup{}
