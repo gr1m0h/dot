5902fc033d7121b7a63c42df42fbcaec6e8d71ed
grimoh <dev@grimoh.net>
2022-11-16 14:50:13 +0900

    Remove copilot, cmp-tabnine

--- .config/nvim/lua/config/nvim-cmp.lua
+++ .config/nvim/lua/config/nvim-cmp.lua
@@ -7,47 +7,45 @@ local luasnip = require("luasnip")
 local lspkind = require("lspkind")
 
 cmp.setup {
-  snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body)
-    end,
-  },
-  mapping = cmp.mapping.preset.insert({
-    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-    ["<C-f>"] = cmp.mapping.scroll_docs(4),
-    ["<C-Space>"] = cmp.mapping.complete(),
-    ["<C-e>"] = cmp.mapping.close(),
-    ["<CR>"] = cmp.mapping.confirm({
-      behavior = cmp.ConfirmBehavior.Replace,
-      select = true
-    }),
-  }),
-  sources = cmp.config.sources({
-    { name = "nvim_lsp" },
-    { name = "nvim_lsp_signature_help" },
-    { name = "nvim_lsp_document_symbol" },
-    { name = "nvim_lua" },
-    { name = "buffer" },
-    { name = "path" },
-    { name = "cmdline" },
-    { name = "luasnip" },
-    { name = "treesitter" },
-    { name = "copilot" },
-    { name = "cmp_tabnine" },
-  }),
-  formatting = {
-    format = lspkind.cmp_format({
-      with_text = true,
-      maxwidth = 50,
-    })
-  },
+	snippet = {
+		expand = function(args)
+			luasnip.lsp_expand(args.body)
+		end,
+	},
+	mapping = cmp.mapping.preset.insert({
+		["<C-d>"] = cmp.mapping.scroll_docs(-4),
+		["<C-f>"] = cmp.mapping.scroll_docs(4),
+		["<C-Space>"] = cmp.mapping.complete(),
+		["<C-e>"] = cmp.mapping.close(),
+		["<CR>"] = cmp.mapping.confirm({
+			behavior = cmp.ConfirmBehavior.Replace,
+			select = true
+		}),
+	}),
+	sources = cmp.config.sources({
+		{ name = "nvim_lsp" },
+		{ name = "nvim_lsp_signature_help" },
+		{ name = "nvim_lsp_document_symbol" },
+		{ name = "nvim_lua" },
+		{ name = "buffer" },
+		{ name = "path" },
+		{ name = "cmdline" },
+		{ name = "luasnip" },
+		{ name = "treesitter" },
+	}),
+	formatting = {
+		format = lspkind.cmp_format({
+			with_text = true,
+			maxwidth = 50,
+		})
+	},
 }
-
--- autopairs
-local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done({ map_char = { tex = "" } }))
 
 vim.g.completeopt = "menu,menuone,noselect"
 vim.cmd [[
   highlight! default link CmpItemKind CmpItemMenuDefault
 ]]
+
+-- autopairs
+local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done({ map_char = { tex = "" } }))
--- .config/nvim/lua/config/nvim-lspconfig.lua
+++ .config/nvim/lua/config/nvim-lspconfig.lua
@@ -15,29 +15,36 @@ local on_attach = function(client, bufnr)
 	end
 end
 
+-- cmp
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
 -- sql
 config.sqls.setup {
 	on_attach = on_attach,
+	capabilities = capabilities,
 }
 -- lua
 config.sumneko_lua.setup {
 	on_attach = on_attach,
+	capabilities = capabilities,
 	settings = {
 		Lua = {
 			diagnostics = {
 				enable = true,
 				globals = { "vim", "use" },
 			},
-		}
+		},
 	},
 }
 -- docker
 config.dockerls.setup {
 	on_attach = on_attach,
+	capabilities = capabilities,
 }
 -- go
 config.gopls.setup {
 	on_attach = on_attach,
+	capabilities = capabilities,
 	cmd = { "gopls", "serve" },
 	settings = {
 		gopls = {
@@ -54,25 +61,30 @@ config.gopls.setup {
 -- terraform
 config.terraformls.setup {
 	on_attach = on_attach,
+	capabilities = capabilities,
 	filetypes = { "terraform", "tf" },
 	cmd = { "terraform-ls", "serve" },
 }
 -- javascript / typescript
 config.tsserver.setup {
 	on_attach = on_attach,
+	capabilities = capabilities,
 	filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
 	cmd = { "typescript-language-server", "--stdio" },
 }
 config.rome.setup {
 	on_attach = on_attach,
+	capabilities = capabilities,
 }
 -- markdown
 config.marksman.setup {
 	on_attach = on_attach,
+	capabilities = capabilities,
 }
 -- yaml
 config.yamlls.setup {
 	on_attach = on_attach,
+	capabilities = capabilities,
 }
 
 set("n", "li", ":LspInfo<Return>", { noremap = true })
