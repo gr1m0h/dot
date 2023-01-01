9987f500fe19650ff2f5dad8568ee071b13da05a
grimoh <dev@grimoh.net>
2023-01-01 20:49:58 +0900

    Update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,7 +6,6 @@ alias ..='cd ..'
 alias ...='cd ../..'
 alias ....='cd ../../..'
 alias c='clear'
-alias teee='tee >(pbcopy)'
 
 set -gx XDG_DATA_HOME $HOME/.local/share
 set -gx XDG_CONFIG_HOME $HOME/.config
