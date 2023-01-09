b80deaca500f5e0af7697cb9915958a6decbfd07
grimoh <dev@grimoh.net>
2023-01-10 06:02:25 +0900

    Update telescope.lua

--- .config/nvim/lua/config/telescope.lua
+++ .config/nvim/lua/config/telescope.lua
@@ -1,46 +1,46 @@
 -- https://github.com/nvim-telescope/telescope.nvim
 
-local status, telescope = pcall(require, "telescope")
+local status, telescope = pcall(require, 'telescope')
 if (not status) then return end
 
-local actions = require("telescope.actions")
-local builtin = require("telescope.builtin")
-local trouble = require("trouble.providers.telescope")
+local actions = require('telescope.actions')
+local builtin = require('telescope.builtin')
+local trouble = require('trouble.providers.telescope')
 
 local function telescope_buffer_dir()
-  return vim.fn.expand("%:p:h")
+  return vim.fn.expand('%:p:h')
 end
 
-local fb_actions = require "telescope".extensions.file_browser.actions
+local fb_actions = require('telescope').extensions.file_browser.actions
 
 telescope.setup {
   defaults = {
     mappings = {
       i = {
-        ["t"] = trouble.open_with_trouble,
+        ['t'] = trouble.open_with_trouble,
       },
       n = {
-        ["t"] = trouble.open_with_trouble,
-        ["q"] = actions.close,
+        ['t'] = trouble.open_with_trouble,
+        ['q'] = actions.close,
       },
     },
   },
   extensions = {
     file_browser = {
-      theme = "dropdown",
+      theme = 'dropdown',
       -- disables netrw and use telescope-file-browser in its place
       hijack_netrw = true,
       mappings = {
         -- your custom insert mode mappings
-        ["i"] = {
-          ["<C-w>"] = function() vim.cmd("normal vbd") end,
+        ['i'] = {
+          ['<C-w>'] = function() vim.cmd('normal vbd') end,
         },
-        ["n"] = {
+        ['n'] = {
           -- your custom normal mode mappings
-          ["N"] = fb_actions.create,
-          ["h"] = fb_actions.goto_parent_dir,
-          ["/"] = function()
-            vim.cmd("startinsert")
+          ['N'] = fb_actions.create,
+          ['h'] = fb_actions.goto_parent_dir,
+          ['/'] = function()
+            vim.cmd('startinsert')
           end
         },
       },
@@ -48,45 +48,46 @@ telescope.setup {
   },
 }
 
-telescope.load_extension("file_browser")
+telescope.load_extension('file_browser')
 
 -- keymaps
-vim.keymap.set("n", "sf", function()
+local set = vim.keymap.set
+set('n', 'fb', function()
   telescope.extensions.file_browser.file_browser({
-    path = "%:p:h",
+    path = '%:p:h',
     cwd = telescope_buffer_dir(),
     respect_gitignore = false,
     hidden = true,
     grouped = true,
     previewer = false,
-    initial_mode = "normal",
+    initial_mode = 'normal',
     layout_config = { height = 40 }
   })
 end)
 
-vim.keymap.set("n", ";f", function()
+set('n', ';f', function()
   builtin.find_files({
     no_ignore = false,
     hidden = true
   })
 end)
 
-vim.keymap.set("n", ";g", function()
+set('n', ';g', function()
   builtin.live_grep()
 end)
 
-vim.keymap.set("n", ";b", function()
+set('n', ';b', function()
   builtin.buffers()
 end)
 
-vim.keymap.set("n", ";t", function()
+set('n', ';t', function()
   builtin.help_tags()
 end)
 
-vim.keymap.set("n", ";r", function()
+set('n', ';r', function()
   builtin.resume()
 end)
 
-vim.keymap.set("n", ";e", function()
+set('n', ';d', function()
   builtin.diagnostics()
 end)
