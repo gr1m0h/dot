3952e2aadd3569e0199e46f1dce8c348dc1eef47
grimoh <dev@grimoh.net>
2023-01-17 14:54:58 +0900

    Update modes.nvim config

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -326,7 +326,7 @@ packer.startup {
 			'mvllow/modes.nvim',
 			tag = 'v0.2.0',
 			config = function()
-				require('modes').setup {}
+				require('config.modes')
 			end
 		}
 		-------------------------------
