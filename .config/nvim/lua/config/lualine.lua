657ffb02039d69bda1255f02017ea65a651ec016
grimoh <dev@grimoh.net>
2023-01-20 04:41:15 +0900

    Update lualine theme

--- .config/nvim/lua/config/lualine.lua
+++ .config/nvim/lua/config/lualine.lua
@@ -6,7 +6,7 @@ if (not status) then return end
 lualine.setup {
 	options = {
 		icon_enabled = true,
-		theme = 'dracula',
+		theme = 'dracula-nvim',
 		section_separators = { left = '', right = '' },
 		component_separators = { left = '', right = '' },
 		disabled_filetypes = {}
