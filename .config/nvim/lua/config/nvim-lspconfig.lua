aebd2fa9835295ec651927efe8e7d72f69ba7330
grimoh <dev@grimoh.net>
2023-01-23 03:59:55 +0900

    Update nvim-lspconfig.lua

--- .config/nvim/lua/config/nvim-lspconfig.lua
+++ .config/nvim/lua/config/nvim-lspconfig.lua
@@ -3,20 +3,20 @@
 local status, config = pcall(require, 'lspconfig')
 if (not status) then return end
 
-local set = vim.keymap.set
 local navic = require('nvim-navic')
+local set = vim.keymap.set
 
 local on_attach = function(client, bufnr)
-	-- format on save
-	if client.server_capabilities.documentFormattingProvider then
-		vim.api.nvim_create_autocmd('BufWritePre', {
-			group = vim.api.nvim_create_augroup('Format', { clear = true }),
-			buffer = bufnr,
-			callback = function() vim.lsp.buf.format() end
-		})
-	end
-	-- navic
-	navic.attach(client, bufnr)
+  -- format on save
+  if client.server_capabilities.documentFormattingProvider then
+    vim.api.nvim_create_autocmd('BufWritePre', {
+      group = vim.api.nvim_create_augroup('Format', { clear = true }),
+      buffer = bufnr,
+      callback = function() vim.lsp.buf.format() end
+    })
+  end
+  -- navic
+  navic.attach(client, bufnr)
 end
 
 -- cmp
@@ -24,72 +24,75 @@ local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 -- sql
 config.sqls.setup {
-	on_attach = on_attach,
-	capabilities = capabilities,
+  on_attach = on_attach,
+  capabilities = capabilities,
 }
 -- lua
 config.sumneko_lua.setup {
-	on_attach = on_attach,
-	capabilities = capabilities,
-	settings = {
-		Lua = {
-			diagnostics = {
-				enable = true,
-				globals = { 'vim', 'use' },
-			},
-		},
-	},
+  on_attach = on_attach,
+  capabilities = capabilities,
+  settings = {
+    Lua = {
+      diagnostics = {
+        enable = true,
+        globals = { 'vim', 'use' },
+      },
+    },
+  },
 }
 -- docker
 config.dockerls.setup {
-	on_attach = on_attach,
-	capabilities = capabilities,
+  on_attach = on_attach,
+  capabilities = capabilities,
 }
 -- go
 config.gopls.setup {
-	on_attach = on_attach,
-	capabilities = capabilities,
-	cmd = { 'gopls', 'serve' },
-	settings = {
-		gopls = {
-			analyses = {
-				unusedparams = true,
-			},
-			staticcheck = true,
-			codelenses = {
-				run_vulncheck_exp = true,
-			},
-		},
-	},
+  on_attach = on_attach,
+  capabilities = capabilities,
+  cmd = { 'gopls', 'serve' },
+  settings = {
+    gopls = {
+      analyses = {
+        unusedparams = true,
+      },
+      staticcheck = true,
+      codelenses = {
+        run_vulncheck_exp = true,
+      },
+    },
+  },
 }
 -- terraform
 config.terraformls.setup {
-	on_attach = on_attach,
-	capabilities = capabilities,
-	filetypes = { 'terraform', 'tf' },
-	cmd = { 'terraform-ls', 'serve' },
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = { 'terraform', 'tf' },
+  cmd = { 'terraform-ls', 'serve' },
 }
 -- javascript / typescript
 config.tsserver.setup {
-	on_attach = on_attach,
-	capabilities = capabilities,
-	filetypes = { 'typescript', 'typescriptreact', 'typescript.tsx' },
-	cmd = { 'typescript-language-server', '--stdio' },
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = { 'typescript', 'typescriptreact', 'typescript.tsx' },
+  cmd = { 'typescript-language-server', '--stdio' },
 }
 config.rome.setup {
-	on_attach = on_attach,
-	capabilities = capabilities,
+  on_attach = on_attach,
+  filetypes = { 'javascript', 'typescript' },
+  capabilities = capabilities,
 }
 -- markdown
 config.marksman.setup {
-	on_attach = on_attach,
-	capabilities = capabilities,
+  on_attach = on_attach,
+  filetypes = { 'markdown' },
+  capabilities = capabilities,
 }
 -- yaml
 config.yamlls.setup {
-	on_attach = on_attach,
-	capabilities = capabilities,
+  on_attach = on_attach,
+  filetypes = { 'yaml' },
+  capabilities = capabilities,
 }
 
-set('n', 'li', ':LspInfo<Return>', { noremap = true })
-set('n', 'ls', ':LspStart<Return>', { noremap = true })
+set('n', 'li', ':LspInfo<Return>', { noremap = true, silent = true })
+set('n', 'ls', ':LspStart<Return>', { noremap = true, silent = true })
