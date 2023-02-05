2cedf5028dd76bacf4bdabcc41d0448362ff56a4
grimoh <dev@grimoh.net>
2023-02-06 03:08:17 +0900

    Fix config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,21 +1,15 @@
 # remove greeting messsage
 set -gx fish_greeting
 
-# prompt
-starship init fish | source
+set -gx XDG_DATA_HOME $HOME/.local/share
+set -gx XDG_CONFIG_HOME $HOME/.config
+set -gx WORKSPACE_HOME $HOME/Documents/workspace
 
 # util
 alias ..='cd ..'
 alias ...='cd ../..'
 alias ....='cd ../../..'
 alias c='clear'
-
-set -gx XDG_DATA_HOME $HOME/.local/share
-set -gx XDG_CONFIG_HOME $HOME/.config
-set -gx WORKSPACE_HOME $HOME/Documents/workspace
-
-# prompt/starship
-set -gx STARSHIP_CONFIG $XDG_CONFIG_HOME/starship/starship.toml
 
 # asdf
 source /opt/homebrew/opt/asdf/libexec/asdf.fish
@@ -26,6 +20,10 @@ set -gx AQUA_GLOBAL_CONFIG $HOME/.aqua.yaml
 set -gx PATH $AQUA_ROOT_DIR/bin $PATH
 
 set -gx EDITOR $AQUA_ROOT_DIR/bin/nvim
+
+# prompt
+starship init fish | source
+set -gx STARSHIP_CONFIG $XDG_CONFIG_HOME/starship/starship.toml
 
 # ghq
 set -gx GHQ_ROOT_DIR $WORKSPACE_HOME
