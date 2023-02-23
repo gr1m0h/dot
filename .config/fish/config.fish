8d8404c56bb50a75aa12592724383150717f0642
grimoh <dev@grimoh.net>
2023-02-23 20:52:52 +0900

    Fix config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -26,7 +26,6 @@ set -gx STARSHIP_CONFIG $XDG_CONFIG_HOME/starship/starship.toml
 # fzf
 set -Ux FZF_DEFAULT_OPTS "--color=fg:#f8f8f2,bg:#282a36,hl:#bd93f9 --color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9 --color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6 --color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4"
 
-
 # ghq
 set -gx GHQ_ROOT_DIR $WORKSPACE_HOME
 set -gx GHQ_SELECTOR fzf
