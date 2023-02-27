87f603fed8138fa2c55b83a08efc55379150c217
grimoh <dev@grimoh.net>
2023-02-27 10:30:28 +0900

    Remove starship

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -20,10 +20,6 @@ set -gx PATH $AQUA_ROOT_DIR/bin $PATH
 
 set -gx EDITOR $AQUA_ROOT_DIR/bin/nvim
 
-# prompt
-starship init fish | source
-set -gx STARSHIP_CONFIG $XDG_CONFIG_HOME/starship/starship.toml
-
 # fzf
 set -Ux FZF_DEFAULT_OPTS "--color=fg:#f8f8f2,bg:#282a36,hl:#bd93f9 --color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9 --color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6 --color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4"
 
