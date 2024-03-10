72851ea694a5096cf62a113d8f27e7bcd2964895
gr1m0h <dev@grimoh.net>
2024-03-10 23:48:11 +0900

    chore: Update neovim plugin
    Update bufferline.nvim, v4.4.1 -> v4.5.2

--- tmp/nvim/lua/plugins.lua
+++ tmp/nvim/lua/plugins.lua
@@ -193,7 +193,7 @@ require('lazy').setup({
   -- buffer line
   {
     'akinsho/bufferline.nvim',
-    tag = 'v4.4.1',
+    tag = 'v4.5.2',
     dependencies = {
       'nvim-tree/nvim-web-devicons',
     },
