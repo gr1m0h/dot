f5c6a14a6fffe9fcfb945503c5fe53870fb213a0
gr1m0h <dev@grimoh.net>
2025-03-03 01:17:05 +0900

    style: Use single quotes for consistency

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,11 +1,9 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
+    'git', 'clone', '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
     lazypath,
   })
 end
@@ -372,7 +370,7 @@ require('lazy').setup({
     end,
   },
   -- {
-  --   "jay-babu/mason-nvim-dap.nvim",
+  --   'jay-babu/mason-nvim-dap.nvim',
   --   tag = 'v2.4.0',
   --   config = function()
   --     require('config.mason-nvim-dap')
