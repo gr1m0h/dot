dd46ad4d6536708b48ebc7665b0fe2965e1debaf
gr1m0h <dev@grimoh.net>
2025-03-03 01:18:32 +0900

    feat: Add new feature to improve user experience

--- .config/nvim/lua/config/copilot.lua
+++ .config/nvim/lua/config/copilot.lua
@@ -5,6 +5,31 @@ if (not status) then return end
 
 
 copilot.setup {
-  suggestion = { enabled = true },
-  pannel = { enabled = true },
+  suggestion = { enabled = false },
+  pannel = { enabled = false },
+  server_opts_overrides = {
+    trace = "verbose",
+    cmd = {
+      vim.fn.expand("~/.config/nvim/copilot/bin/copilot-language-server"),
+      "--stdio"
+    },
+    settings = {
+      advanced = {
+        listCount = 10,
+        inlineSuggestCount = 3,
+      },
+    },
+  },
+  filetypes = {
+    yaml = true,
+    markdown = true,
+    help = false,
+    gitcommit = true,
+    gitrebase = true,
+    hgcommit = false,
+    svn = false,
+    cvs = false,
+    ["."] = false,
+    ["*"] = true,
+  },
 }
