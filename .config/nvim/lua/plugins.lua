4f4d57472ba47b70d5d4ebcf4254f40cb2103bb7
gr1m0h <dev@grimoh.net>
2025-03-09 21:15:23 +0900

    feat: Add avante.nvim with copilot integration

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -343,6 +343,73 @@ require('lazy').setup({
       require('config.CopilotChat')
     end,
   },
+  {
+    'yetone/avante.nvim',
+    event = 'VeryLazy',
+    lazy = false,
+    version = false,
+    opts = {
+      provider = 'copilot',
+      auto_suggestions_provider = "copilot",
+      behaviour = {
+        auto_suggestions = true,
+        auto_set_highlight_group = true,
+        auto_set_keymaps = true,
+        auto_apply_diff_after_generation = true,
+        support_paste_from_clipboard = true,
+      },
+      windows = {
+        position = "right",
+        width = 30,
+        sidebar_header = {
+          align = "center",
+          rounded = false,
+        },
+        ask = {
+          floating = true,
+          start_insert = true,
+          border = "rounded"
+        }
+      },
+      copilot = {
+        model = "gpt-4o-2024-05-13",
+        -- model = "gpt-4o-mini",
+        max_tokens = 4096,
+      },
+    },
+    build = 'make',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+      'stevearc/dressing.nvim',
+      'nvim-lua/plenary.nvim',
+      'MunifTanjim/nui.nvim',
+      --- The below dependencies are optional,
+      'nvim-tree/nvim-web-devicons',
+      'zbirenbaum/copilot.lua',
+      {
+        -- support for image pasting
+        'HakonHarnes/img-clip.nvim',
+        event = 'VeryLazy',
+        opts = {
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+          },
+        },
+      },
+      {
+        -- Make sure to set this up properly if you have lazy=true
+        'MeanderingProgrammer/render-markdown.nvim',
+        opts = {
+          file_types = { 'markdown', 'Avante' },
+        },
+        ft = { 'markdown', 'Avante' },
+      },
+    },
+  },
   -------------------------------------------------
   --
   -- Debug Adapter Protocol
